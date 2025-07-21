c2b2b4af93ba54ca8774af2fcb009112078af159
Jarr1t <71787522+Jarr1t@users.noreply.github.com>
2025-07-21 12:31:00 -0400

    adding solution to twosum.js

--- arrays/easy/twosum.js
+++ arrays/easy/twosum.js
@@ -1,1 +1,18 @@
-//two sum solution coming soon
+// 001. Two Sum
+// https://leetcode.com/problems/two-sum/
+// Difficulty: Easy
+// Time: O(n), Space: O(n)
+
+function twoSum(nums, target) {
+  const differences = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (differences.has(nums[i])) {
+      return [differences.get(nums[i]), i];
+    } else {
+      differences.set(diff, i);
+    }
+  }
+}
