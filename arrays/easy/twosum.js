d274280c34465473b8cdd42af2969cdb6b5165c1
Jarr1t <71787522+Jarr1t@users.noreply.github.com>
2025-07-22 23:56:08 -0400

    documenting the two sum algorithm

--- arrays/easy/twosum.js
+++ arrays/easy/twosum.js
@@ -1,18 +1,18 @@
 // 001. Two Sum
-// https://leetcode.com/problems/two-sum/
+// https://neetcode.io/problems/two-integer-sum?list=neetcode150
 // Difficulty: Easy
 // Time: O(n), Space: O(n)
 
 function twoSum(nums, target) {
-  const differences = new Map();
+  const differences = new Map(); // a Map to hold key: value pairs of differences and the indexes of the corresponding numbers (differences: i)
 
-  for (let i = 0; i < nums.length; i++) {
-    let diff = target - nums[i];
+  for (let i = 0; i < nums.length; i++) { // iteate through the nums array
+    let diff = target - nums[i]; // find the difference between the given target number and the current number in the array
 
-    if (differences.has(nums[i])) {
-      return [differences.get(nums[i]), i];
+    if (differences.has(nums[i])) { // checking if the differences object contains the number I'm currently looking at in the array
+      return [differences.get(nums[i]), i]; // if true, return the index of the difference within the differences Map and the current index
     } else {
-      differences.set(diff, i);
+      differences.set(diff, i); // if false, add the calculated difference and the current index to the Map
     }
   }
 }
